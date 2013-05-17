b4718fe793e24cb2ec9b215b5f435e0daeb6b271
Velin Koychev <kojchev@gmail.com>
2013-05-17 23:16:13 +0300

    Demo commit

---
+++ ConsoleApplication1/TestLogging.cs
@@ -0,0 +1,24 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class TestLogging
+    {
+        private static readonly ILog testLoger =
+    LogManager.GetLogger(typeof(TestLogging));
+
+        static void Main(string[] args)
+        {
+            BasicConfigurator.Configure();
+            testLoger.Debug("Debug msg");
+            testLoger.Error("Error msg");
+
+        }
+    }
+}
---
+++ ConsoleApplication1/TextTemplate1.cs
@@ -0,0 +1,10 @@
+﻿namespace TestTemplateGeneratedFile
+{
+	class DemoClass
+	{
+		public int TelerikDemo0 { get; set; }
+		public int TelerikDemo1 { get; set; }
+		public int TelerikDemo2 { get; set; }
+	   // I am sure that this is the best demo template ever made
+	}
+}
